ada2eff8e505645429f3e508076114f323348591
Malcom <marcosino654@gmail.com>
2023-10-09 09:25:03 +0200

    Main 1.1

--- main.java
+++ main.java
@@ -1,4 +1,7 @@
 public static void Main(String[] args){
 	String salutation = "HOLA MUNDO";
 	System.out.println(salutation);
+	String farewell = "Adios con el corasao";
+	System.out.println(farewell);
+
 }
