c7baaef63ad56851f0532912a1d7f033c63a21f0
Malcom <marcosino654@gmail.com>
2023-10-09 10:22:09 +0200

    addNumbers

--- main.java
+++ main.java
@@ -4,4 +4,13 @@ public static void Main(String[] args){
 	String farewell = "Adios con el corasao";
 	System.out.println(farewell);
 
+	int num1 = 4;
+	int num2 = 5;
+
+	System.out.println(addNumbers(num1, num2);
 }
+
+public static int addNumbers(int a, int b){
+	return (a+b);
+}
+
